5e68fb238fe6904822fc2d412386e74fc28b6de1
AhadTariqCaptainX <ahad.imvu@gmail.com>
2025-07-24 07:48:51 +0500

    Refactor to add event listener

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-function saveLead(){
-    console.log("Button Clicked")
-}
+
+
+let inputBtn = document.getElementById("input-btn")
+inputBtn.addEventListener("click", function(){
+    console.log("Button Click inside Event listener")
+})
