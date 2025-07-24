61400a44f39263da72d7ff659498bdc37b24fffc
AhadTariqCaptainX <ahad.imvu@gmail.com>
2025-07-24 19:24:07 +0500

    Improving code reusability by function parameter

--- index.js
+++ index.js
@@ -8,30 +8,30 @@ const leadFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if(leadFromLocalStorage) {
     myLeads = leadFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
 
 deleteBtn.addEventListener("dblclick", function(){
     localStorage.clear()
     myLeads = []
-    renderLeads()
+    render(myLeads)
 })
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
+    render(myLeads)
 })
 
-function renderLeads(){
+function render(leads){
     let listItems = " "
-    for(let i=0; i<myLeads.length; i++){
+    for(let i=0; i<leads.length; i++){
         // listItems += "<li><a target = '_blank' href='"+ inputEl.value+ "'>"  + myLeads[i] + "</a></li>" 
         listItems += `
         <li>
-            <a target="_blank" href="${myLeads[i]}">
-                ${myLeads[i]}
+            <a target="_blank" href="${leads[i]}">
+                ${leads[i]}
             </a>
         </li>
         `
