ca641433e2aeaf103c1633f61462fed0b8d11f83
AhadTariqCaptainX <ahad.imvu@gmail.com>
2025-07-24 20:56:34 +0500

    final Deployement

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
-
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-const leadFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
 
-if(leadFromLocalStorage) {
-    myLeads = leadFromLocalStorage
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
     render(myLeads)
 }
 
-deleteBtn.addEventListener("dblclick", function(){
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+
+deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear()
     myLeads = []
     render(myLeads)
 })
 
-inputBtn.addEventListener("click", function(){
+inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     render(myLeads)
 })
-
-function render(leads){
-    let listItems = " "
-    for(let i=0; i<leads.length; i++){
-        // listItems += "<li><a target = '_blank' href='"+ inputEl.value+ "'>"  + myLeads[i] + "</a></li>" 
-        listItems += `
-        <li>
-            <a target="_blank" href="${leads[i]}">
-                ${leads[i]}
-            </a>
-        </li>
-        `
-      
-        // create element, set text content, append to ul
-        // const li = document.createElement("li")
-        // li.textContent = myLeads[i]
-        // ulEl.append(li)
-    }
-    ulEl.innerHTML = listItems
-
-}
-
