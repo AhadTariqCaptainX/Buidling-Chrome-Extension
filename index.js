e2d7bf239932bcc5fbee625562f4883db4fff25f
AhadTariqCaptainX <ahad.imvu@gmail.com>
2025-07-24 12:52:14 +0500

    create the render function

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 
-let myLeads = ["a", "b", "c"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
+    renderLeads()
 })
 
-let listItems = " "
-for(let i=0; i<myLeads.length; i++){
-    listItems += "<li>" + myLeads[i] + "</li>" 
-    // create element, set text content, append to ul
-    // const li = document.createElement("li")
-    // li.textContent = myLeads[i]
-    // ulEl.append(li)
+function renderLeads(){
+    let listItems = " "
+    for(let i=0; i<myLeads.length; i++){
+        listItems += "<li>" + myLeads[i] + "</li>" 
+        // create element, set text content, append to ul
+        // const li = document.createElement("li")
+        // li.textContent = myLeads[i]
+        // ulEl.append(li)
+    }
+    ulEl.innerHTML = listItems
+
 }
-ulEl.innerHTML = listItems
+
