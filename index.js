df5653501a70b035f08f1e1b294e43b701d49964
AhadTariqCaptainX <ahad.imvu@gmail.com>
2025-07-24 08:48:26 +0500

    Redering list of leads using unordered list

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 
-let myLeads = []
+let myLeads = ["a", "b", "c"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
 })
 
 for(let i=0; i<myLeads.length; i++){
-    console.log(myLeads[i])
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>" 
 }
